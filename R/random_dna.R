2e34db194d1d260756546a9f33ce3559ddce4964
HenrikSpiegel <henrik.spiegelhauer@gmail.com>
2021-03-22 09:33:52 +0100

    Random_dna documentation

--- R/random_dna.R
+++ R/random_dna.R
@@ -1,5 +1,17 @@
-random_dna <- function(l){
-  nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
+#' Random DNA sequence
+#'
+#' Creates a n long sequence of random nucleotides.
+#'
+#' @param l integer
+#'
+#' @return dna string
+#' @export
+#'
+#' @examples
+#' random_dna(10)
+
+random_dna <- function(n){
+  nucleotides <- sample(c("A", "T", "G", "C"), size = n, replace = TRUE)
   dna = paste0(nucleotides, collapse = "")
   return(dna)
 }
